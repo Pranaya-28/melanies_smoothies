be32dcffb15428db7f3eaaab85e8b339a2da0057
Pranaya-28 <85429318+Pranaya-28@users.noreply.github.com>
2024-06-19 15:10:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('search_on'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
-st.dtaframe(pd_df)
+st.dataframe(pd_df)
 st.stop()
 search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
